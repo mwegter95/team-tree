01df5ace41efbb587defd2a58f3ac4ba015c8ba9
hitmaker <mwegter95@gmail.com>
2021-12-20 00:08:50 -0600

    manager is prompted and returning a classed manager object

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+const Manager = require('./lib/Manager');
+const Employee = require('./lib/Employee');
+const createTeam = require('./src/create-team');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -15,8 +18,50 @@ const promptUser = () => {
                     return false;
                 }
             }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is your employee ID?',
+            validate: idInput => {
+                if (typeof(idInput=Number)) {
+                    return true;
+                } else {
+                    console.log('Please enter your ID number as a number!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your email address!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'office',
+            message: 'What is your office number?',
+            validate: officeInput => {
+                if (officeInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your office number!');
+                    return false;
+                }
+            }
         }
     ])
 };
 
-promptUser()
+promptUser().then(answers => {
+    const manager = new Manager(answers.name, answers.id, answers.email, answers.office);
+    console.log(manager);
+})
